c648370a64fce525ba22f5db54f8770f1a47a9fe
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-01 07:26:30 +0100

    Created a Constants class.

--- app/src/main/java/com/example/getmapaddress/FetchAddressIntentService.java
+++ app/src/main/java/com/example/getmapaddress/FetchAddressIntentService.java
@@ -2,10 +2,14 @@ package com.example.getmapaddress;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.ResultReceiver;
 
 import androidx.annotation.Nullable;
 
 public class FetchAddressIntentService extends IntentService {
+
+    private ResultReceiver resultReceiver;
+
     public FetchAddressIntentService() {
         super("FetchAddressIntentService");
     }
