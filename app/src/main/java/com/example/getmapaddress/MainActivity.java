11ae5fae07844de5c795a5c21133f89761575749
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-01 17:24:00 +0100

    Completed application.

--- app/src/main/java/com/example/getmapaddress/MainActivity.java
+++ app/src/main/java/com/example/getmapaddress/MainActivity.java
@@ -7,9 +7,13 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.ResultReceiver;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,16 +24,21 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.net.Inet4Address;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
     private TextView textLatLong, textAddress;
     private ProgressBar progressBar;
+    private ResultReceiver resultReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultReceiver = new AddressResultReceiver(new Handler());
 
         textLatLong = findViewById(R.id.textLatLong);
         progressBar = findViewById(R.id.progressBar);
@@ -104,11 +113,42 @@ public class MainActivity extends AppCompatActivity {
                                             longitude
                                     )
                             );
-                        }
 
-                        progressBar.setVisibility(View.GONE);
+                            Location location = new Location("providerNA");
+                            location.setLatitude(latitude);
+                            location.setLongitude(longitude);
+                            fetchAddressFromLatLong(location);
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+                        }
                     }
                 }, Looper.getMainLooper());
 
+    }
+
+    private void fetchAddressFromLatLong(Location location) {
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, resultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+        startService(intent);
+    }
+
+    private class AddressResultReceiver extends ResultReceiver {
+
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            super.onReceiveResult(resultCode, resultData);
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                textAddress.setText(resultData.getString(Constants.RESULT_DATA_KEY));
+            } else {
+                Toast.makeText(MainActivity.this, resultData.getString(Constants.RESULT_DATA_KEY), Toast.LENGTH_SHORT).show();
+            }
+
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
